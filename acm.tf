c740793e4078f0c2acc54d96bf69297db76fe2df
valkaitibor <125676234+valkaitibor@users.noreply.github.com>
2023-03-28 16:00:34 +0200

    wordpress4

---
+++ acm.tf
@@ -0,0 +1,14 @@
+# AWS Certificate Manager Terraform module
+
+module "acm" {
+  source      = "terraform-aws-modules/acm/aws"
+  version     = "~> v4.0.1"
+  domain_name = var.site_domain
+  zone_id     = var.cloudflare_zone
+
+  ##### cloudflare
+  create_route53_records  = false
+  validation_record_fqdns = cloudflare_record.validation.*.hostname
+
+  tags = var.tags
+}
